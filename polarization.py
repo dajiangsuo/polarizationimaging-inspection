8f4abfac037cf81540070ff85ee44a4b9f6d57ed
aarongs98 <aarongs@mit.edu>
2020-08-11 18:34:38 -0500

    updated code

--- polarization.py
+++ polarization.py
@@ -3,14 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #function to display the different image types
-def displayImages(Intensity, AoP, DoLP, HSVinBGR):
+#if edges is true show the edges for each image
+def displayImages(Intensity, AoP, DoLP, HSVinBGR, edges):
     images = [Intensity, AoP, DoLP, HSVinBGR]
-    plot_titles = ['1-channel: Intensity', '1-channel: AoP', '1-channel: DoLP', 'HSV Image in BGR']
+    if edges:
+        display_images = []
+        plot_titles = ['Intensity: edges', 'AoP: edges', 'DoLP: edges', 'HSV Image in BGR: edges']
+        for img in images:
+            display_images.append(cv2.Canny(img,100,200))
+    else:
+        display_images = images
+        plot_titles = ['1-channel: Intensity', '1-channel: AoP', '1-channel: DoLP', 'HSV Image in BGR']
     rows = 2
     cols = 2
     plt.figure("Polarization Parameters")
     for i in range(rows*cols):
-        img = images[i]
+        img = display_images[i]
         subplot_title = plot_titles[i]
         plt.subplot(2,2,i+1)
         plt.imshow(img)
@@ -36,21 +44,24 @@ DoLP = Intensity/I
 AoP = 0.5*np.arctan2(U,Q)
 
 #Use polarization info to convert to HSV
-H = np.uint8(AoP*(180/np.pi))
+H = np.uint8((AoP+np.pi/2)*(180/np.pi))
 S = np.uint8(255*(DoLP/np.amax(DoLP)))
 V = np.uint8(255*(Intensity/np.amax(Intensity)))
 HSV = cv2.merge((H,S,V))
 new_image = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)
+edges = cv2.Canny(new_image, 100, 200)
+edges_image0 = cv2.Canny(np.uint8(image0), 100, 200)
 
 #Extract individual channels
 cv2.imwrite('intensity.jpg', Intensity)
 cv2.imwrite('DoLP.jpg', 255*DoLP)
 cv2.imwrite('AoP.jpg', 255*AoP)
-
+cv2.imwrite('edges_image0.jpg', edges_image0)
 height, width = new_image.shape[:2]
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', width, height)
 cv2.imshow('image', new_image)
 cv2.waitKey(0)
 
-displayImages(Intensity, AoP, DoLP, new_image) #call the displayImages function
+displayImages(Intensity, AoP, DoLP, new_image, False)  #call displayimages to show each channel
+# displayImages(Intensity, AoP, DoLP, new_image, True) #call displayimages to show edges of each channel
